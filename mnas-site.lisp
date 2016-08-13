d2503379181050f05b85ebe1836b0df1723a821f
Nick Matvyeyev <mnasoft@gmail.com>
2016-08-13 20:33:58 +0200

    Добавлена переменная *mnas-site-port*. Добавлены  ip -списки

--- mnas-site.lisp
+++ mnas-site.lisp
@@ -6,11 +6,16 @@
 
 (setf (html-mode) :HTML5)
 
+(defparameter *mnas-site-document-root* "/home/namatv/public_html/")
+
 (defparameter *mnas-site-acceptor* nil)
+
+(defparameter *mnas-site-port* 8000)
 
 (defun mnas-site-start()
   (if (null *mnas-site-acceptor*)
-      (setf *mnas-site-acceptor* (start (make-instance 'easy-acceptor :port 8000)))))
+      (progn (setf *mnas-site-acceptor* (start (make-instance 'easy-acceptor :port *mnas-site-port* )))
+	     (mnas-site-set-document-root))))
 
 (defun mnas-site-stop()
   (if *mnas-site-acceptor*
@@ -30,21 +35,27 @@
 	    (push pr-disp ,disp-tbl))))
 
 (defun allowed-address-p()
-  (member (real-remote-addr)  (allowed-address-list) :test #'equal))
+  (let ((m-inst (machine-instance)))
+    (cond
+      ((string= m-inst "mnasoft-pi") t) ;; (list "127.0.0.1" "192.168.0.100" "192.168.0.101" "192.168.0.102" "192.168.0.103" "192.168.0.110")
+      ((string= m-inst "mnasoft-00")
+       (append *localhost-ip*  *mnasoft-comps-ip*))
+      (t (member (real-remote-addr)  (allowed-address-list) :test #'equal)))))
 
 (defun allowed-address-list()
   (let ((m-inst (machine-instance)))
     (cond
       ((or (string= m-inst "mnasoft-00") (string= m-inst "mnasoft-pi"))
-       '(list "127.0.0.1" "192.168.0.100" "192.168.0.101" "192.168.0.102" "192.168.0.103" "192.168.0.110" )
-       )
-      ((or (string= m-inst "hp1"))
-         (apply #'append 
-	 (mapcar #'(lambda (el) (ip-by-name el)) 
-		 (append
-		  *localhost*
-		  *dep11-comps*
-		  *dep-oakts-comps*)))))))
+       (append *localhost-ip*  *mnasoft-comps-ip*))
+      ((or (string= m-inst "hp1.zorya.com"))
+       (apply
+	#'append 
+	(mapcar
+	 #'(lambda (el) (ip-by-name el)) 
+	 (append
+	  *localhost*
+	  *dep11-comps*
+	  *dep-oakts-comps*)))))))
 
-
- 
+(defun mnas-site-set-document-root ()
+  (setf (acceptor-document-root *mnas-site-acceptor*) *mnas-site-document-root*))
