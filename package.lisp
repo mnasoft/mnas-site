e8a6ab7f79426f3f33706f6d740be120d5b05bf4
Mykola Matvyeyev <mnasoft@gmail.com>
2023-03-12 20:05:19 +0200

    Sun 12 Mar 2023 06:05:19 PM UTC

--- package.lisp
+++ package.lisp
@@ -12,8 +12,4 @@
 	   allowed-address-list
 	   ))
 
-;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
-
-
-
-;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
+(in-package :mnas-site)
